6159c66bf24983c6fdf6876767722b0c3ea914ec
Eng/Yousef Yahia <belyngham61@gmail.com>
2024-12-06 17:47:52 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    window.onscroll = () => {
+        if (window.scrollY > 50) {
+            document.querySelector("#mainNavBar").classList.add("navbarDown");
+            document.querySelector("#logoImg").setAttribute("src", "/images/icons/logo2.png");
+        } else {
+            document.querySelector("#mainNavBar").classList.remove("navbarDown");
+            document.querySelector("#logoImg").setAttribute("src", "/images/icons/logo.png");
+        }
+
+        if (window.scrollY > 100) {
+            document.querySelector("#upArrow").style.display = "block";
+        } else {
+            document.querySelector("#upArrow").style.display = "none";
+        }
+    };
+
+    document.querySelector("#upArrow").addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
