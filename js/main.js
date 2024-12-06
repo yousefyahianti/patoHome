53886e3c8a127fede97a7a15e78b5f9070191d2e
Eng/Yousef Yahia <belyngham61@gmail.com>
2024-12-06 23:42:48 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     window.onscroll = () => {
         if (window.scrollY > 50) {
             document.querySelector("#mainNavBar").classList.add("navbarDown");
-            document.querySelector("#logoImg").setAttribute("src", "/images/icons/logo2.png");
+            document.querySelector("#logoImg").setAttribute("src", "images/icons/logo2.png");
         } else {
             document.querySelector("#mainNavBar").classList.remove("navbarDown");
-            document.querySelector("#logoImg").setAttribute("src", "/images/icons/logo.png");
+            document.querySelector("#logoImg").setAttribute("src", "images/icons/logo.png");
         }
 
         if (window.scrollY > 100) {
